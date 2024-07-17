b21e9c024536ed5353c89f894c67b5f30009955b
adamalcantara <alcantaradevelopment@gmail.com>
2024-07-16 21:10:52 -0400

    Web nav has been styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    const logoText = "<a>";
+    const logoText = "<Adam />";
   return (
-    <div>
-        <div className='text-5xl'>{logoText}</div>
-        <nav>
-            <Link to="/">Portfolio</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
+    <div className='flex justify-between items-center h-[55px]'>
+        <div className='text-4xl ml-5'>{logoText}</div>
+        <nav className='mr-5 text-3xl'>
+            <Link to="/" className='ml-10'>Portfolio</Link>
+            <Link to="/about" className='ml-10'>About</Link>
+            <Link to="/contact" className='ml-10'>Contact</Link>
         </nav>
     </div>
   )
