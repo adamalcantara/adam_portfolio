1fe3033a36d755dfd79849848d494378e60e4333
adamalcantara <alcantaradevelopment@gmail.com>
2024-07-16 20:59:55 -0400

    Routes working properly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
+// import pages
+import Portfolio from './Pages/Portfolio';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <div id='pageContent'>
-        <Route exact path="" component={}/>
+        <Routes>
+          <Route exact path="/" Component={Portfolio} />
+          <Route exact path="/about" Component={About} />
+          <Route exact path="/contact" Component={Contact} />
+        </Routes>
       </div>
     </Router>
   );
