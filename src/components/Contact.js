588847e8e0fc2e56a48a98a8c3fc02f092495886
adamalcantara <alcantaradevelopment@gmail.com>
2024-08-06 00:16:20 -0400

    Updated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,13 @@ const Contact = () => {
   return (
       <div className='dark:bg-zinc-900 pb-10' id='contact'>
         <p className='text-5xl pt-10 mb-10 text-center dark:text-white'>Contact Me</p>
-        <div className='mx-auto w-4/5 md:w-3/5 2xl:w-2/5'>
-          <ContactForm />
+        <div className='mx-auto flex flex-col-reverse md:flex-row justify-between w-3/5'>
+          <div className='md:w-1/2 md:mr-5'>
+            <ContactForm />
+          </div>
+          <div className='md:w-1/2 md:ml-5'>
+            <p className='text-[20px]'>Should you have any questions about me or any of the projects listed here, please feel free to contact me. Thank you so much for stopping by my website, and I hope you enjoyed my portfolio!</p>
+          </div>
         </div>
       </div>
   )
